73b08b0d7acd99271922de07d1f8788247805564
antking19 <cuong.truongkien@gmail.com>
2024-11-30 19:50:45 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,19 @@
 import "./style.css";
 const MyComponent = () => {
+    // const hoidanit = "eric 1";
+    // const hoidanit = 25;
+    // const hoidanit = true;
+    // const hoidanit = undefined;
+    // const hoidanit = null;
+    const hoidanit = [1, 2, 3];
+    // const hoidanit = {
+    //     name: "hoidanit",
+    //     age: 25,
+    // };
     return (
         <>
-            <div>Eric & hoidanit update</div>
+            <div>{JSON.stringify(hoidanit)} & hoidanit update</div>
+            <div>{console.log("ERIC")}</div>
             <div className="child" style={{ borderRadius: "10px" }}>
                 Child
             </div>
