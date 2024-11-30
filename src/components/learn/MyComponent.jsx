8a46472f9a3f18b45cfccf014a583266da11762c
antking19 <cuong.truongkien@gmail.com>
2024-11-30 19:37:46 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,5 +1,13 @@
+import "./style.css";
 const MyComponent = () => {
-    return <div>Eric & hoidanit update</div>;
+    return (
+        <>
+            <div>Eric & hoidanit update</div>
+            <div className="child" style={{ borderRadius: "10px" }}>
+                Child
+            </div>
+        </>
+    );
 };
 
 export default MyComponent;
