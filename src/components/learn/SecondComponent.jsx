da6dcaab0711a48ed4d8c68a0e41037627cd8f91
antking19 <cuong.truongkien@gmail.com>
2024-12-29 11:55:27 +0700

    video29

---
+++ src/components/learn/SecondComponent.jsx
@@ -0,0 +1,9 @@
+const SecondComponent = () => {
+    return <div>second component</div>;
+};
+
+const ThirdComponent = () => {
+    return <div>second component</div>;
+};
+
+export { SecondComponent, ThirdComponent };
