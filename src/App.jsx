26a4d45d15f037be0b22efc364fa3f226311d90e
antking19 <cuong.truongkien@gmail.com>
2024-12-30 21:31:56 +0700

    video 35

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,14 @@ const App = () => {
         address: "hanoi",
         country: "vietnam",
     };
+
+    const addNewTodo = (name) => {
+        alert(`call me ${name}`);
+    };
     return (
         <div className="todo-container">
             <div className="todo-title">Todo List</div>
-            <TodoNew />
+            <TodoNew addNewTodo={addNewTodo} />
             <TodoData name={hoidanit} age={age} data={data} />
             <div className="todo-image">
                 <img src={reactLogo} alt="" className="logo" />
--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,4 +1,7 @@
-const TodoNew = () => {
+const TodoNew = (props) => {
+    console.log(">>> Check props: ", props);
+    const { addNewTodo } = props;
+    addNewTodo("eric");
     return (
         <div className="todo-new">
             <input type="text" placeholder="Enter your task" />
